ea40cca2506dea5d733c6e11ff2e44344da55787
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2017-11-02 14:30:09 -0400

    Working on firebase

--- app/src/main/java/com/gregoryzuroff/coffeebuzz/LoginSignUp.java
+++ app/src/main/java/com/gregoryzuroff/coffeebuzz/LoginSignUp.java
@@ -2,12 +2,36 @@ package com.gregoryzuroff.coffeebuzz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginSignUp extends Activity {
-
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 }
