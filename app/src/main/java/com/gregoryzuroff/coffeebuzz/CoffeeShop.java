28cfc41d66a6959aeef01bfa274a3e9ee27a4da8
Gregory Zuroff <gregoryzuroff@0587374187.wireless.umich.net>
2017-11-28 15:07:54 -0500

    Worked on showing coffee shops in ListView on HomePage

--- app/src/main/java/com/gregoryzuroff/coffeebuzz/CoffeeShop.java
+++ app/src/main/java/com/gregoryzuroff/coffeebuzz/CoffeeShop.java
@@ -9,10 +9,66 @@ import java.util.Hashtable;
  */
 
 public class CoffeeShop {
-    int ID, noiseLevel;
-    double avgOverall, avgVariety, avgStudying, avgLight, avgAccess;
-    String name, atmosphere;
+    int id;
+    int noiseLevel;
+    double avgOverall;
+    double avgVariety;
+    double avgStudying;
+    double avgLight;
+    double avgAccess;
+    String name;
+    String atmosphere;
+    double longitude, latitude;
     double[] gps = new double[2];
-    ArrayList<Drink> menu;
-    Hashtable<String, ArrayList<CoffeeShopReview>> reviews;
+
+    public void setLongitude(double longitude){
+        this.longitude = longitude;
+        this.gps[0] = this.longitude;
+    }
+
+    public void setLatitude(double latitude){
+        this.latitude = latitude;
+        this.gps[1] = this.latitude;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNoiseLevel(int noiseLevel) {
+        this.noiseLevel = noiseLevel;
+    }
+
+    public void setAvgOverall(double avgOverall) {
+        this.avgOverall = avgOverall;
+    }
+
+    public void setAvgVariety(double avgVariety) {
+        this.avgVariety = avgVariety;
+    }
+
+    public void setAvgStudying(double avgStudying) {
+        this.avgStudying = avgStudying;
+    }
+
+    public void setAvgLight(double avgLight) {
+        this.avgLight = avgLight;
+    }
+
+    public void setAvgAccess(double avgAccess) {
+        this.avgAccess = avgAccess;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAtmosphere(String atmosphere) {
+        this.atmosphere = atmosphere;
+    }
+
+    //ArrayList<Drink> menu;
+    //Hashtable<String, ArrayList<CoffeeShopReview>> reviews;
+
+
 }
--- app/src/main/java/com/gregoryzuroff/coffeebuzz/HomePage.java
+++ app/src/main/java/com/gregoryzuroff/coffeebuzz/HomePage.java
@@ -4,16 +4,59 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 public class HomePage extends Activity {
+    private ListView coffeeList;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Shops");
+// Read from the database
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get all shops data as one long string
+                ArrayList<CoffeeShop> shops = new ArrayList<CoffeeShop>();
+                ArrayList<String> names = new ArrayList<String>();
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    shops.add(postSnapshot.getValue(CoffeeShop.class));
+                    names.add(postSnapshot.getValue(CoffeeShop.class).name);
+                }
+                coffeeList = (ListView) findViewById(R.id.coffeeList);
+
+                listAdapter = new ArrayAdapter<String>(HomePage.this, R.layout.simplerow, names);
+                coffeeList.setAdapter( listAdapter );
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
 
     }
 
